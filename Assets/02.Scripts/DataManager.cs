d387e82ab00cb10280de56fa0952d938aab920ec
VillainYami <109275573+VillainYami@users.noreply.github.com>
2024-10-23 15:46:14 +0900

    Title & Save and Load(Json) Update

---
+++ Assets/02.Scripts/DataManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance;
+    public PlayerData nowPlayer = new PlayerData();
+    public string path;
+    public int nowSlot;
+    public class PlayerData
+    {
+        public string name;
+        public int level = 1;
+        public int coin = 1;
+    }
+
+    private void Awake()
+    {
+        #region ΩÃ±€≈Ê
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(instance.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+        #endregion
+        path = Application.persistentDataPath + "/save";
+    }
+
+    void Start()
+    {
+        
+    }
+    public void SaveData()
+    {
+        string data = JsonUtility.ToJson(nowPlayer);
+        File.WriteAllText(path + nowSlot.ToString(), data);
+    }
+
+    public void LoadData()
+    {
+        string data = File.ReadAllText(path + nowSlot.ToString());
+        nowPlayer = JsonUtility.FromJson<PlayerData>(data);
+    }
+    public void DataClear()
+    {
+        nowSlot = -1;
+        nowPlayer = new PlayerData();
+    }
+}
--- Assets/02.Scripts/Title.cs
+++ Assets/02.Scripts/Title.cs
@@ -1,22 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Title : MonoBehaviour
 {
-
-    public void StartGameBT()
+    public GameObject creat;	
+    public TMP_Text[] slotText;		
+    public TMP_Text newPlayerName;
+    bool[] savefile = new bool[4];
+    void Start()
     {
+        // 슬롯별로 저장된 데이터가 존재하는지 판단.
+        for (int i = 0; i < 4; i++)
+        {
+            if (File.Exists(DataManager.instance.path + $"{i}"))	// 데이터가 있는 경우
+            {
+                savefile[i] = true;			// 해당 슬롯 번호의 bool배열 true로 변환
+                DataManager.instance.nowSlot = i;	// 선택한 슬롯 번호 저장
+                DataManager.instance.LoadData();	// 해당 슬롯 데이터 불러옴
+                slotText[i].text = DataManager.instance.nowPlayer.name;	// 버튼에 닉네임 표시
+            }
+            else	// 데이터가 없는 경우
+            {
+                slotText[i].text = "비어있음";
+            }
+        }
+        // 불러온 데이터를 초기화시킴.(버튼에 닉네임을 표현하기위함이었기 때문)
+        DataManager.instance.DataClear();
+    }
+    public void Slot(int number)	// 슬롯의 기능 구현
+    {
+        DataManager.instance.nowSlot = number;	// 슬롯의 번호를 슬롯번호로 입력함.
 
+        if (savefile[number])	// bool 배열에서 현재 슬롯번호가 true라면 = 데이터 존재한다는 뜻
+        {
+            DataManager.instance.LoadData();	// 데이터를 로드하고
+            GoGame();	// 게임씬으로 이동
+        }
+        else	// bool 배열에서 현재 슬롯번호가 false라면 데이터가 없다는 뜻
+        {
+            Creat();	// 플레이어 닉네임 입력 UI 활성화
+        }
     }
 
-    public void LoadGameBT()
+    public void Creat()	// 플레이어 닉네임 입력 UI를 활성화하는 메소드
     {
-
+        creat.gameObject.SetActive(true);
     }
 
-    public void OpsionBT()
+    public void GoGame()	// 게임씬으로 이동
     {
-
+        if (!savefile[DataManager.instance.nowSlot])	// 현재 슬롯번호의 데이터가 없다면
+        {
+            DataManager.instance.nowPlayer.name = newPlayerName.text; // 입력한 이름을 복사해옴
+            DataManager.instance.SaveData(); // 현재 정보를 저장함.
+        }
+        SceneManager.LoadScene(1); // 게임씬으로 이동
     }
+
 }
